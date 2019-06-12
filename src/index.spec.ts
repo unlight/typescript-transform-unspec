fd6ab74e68d03109904e9ceba13b6fb36a66a5e4
roman.vasilev <s91630277@gmail.com>
2019-06-13 00:22:34 +0400

    Refactored tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,19 +1,22 @@
 /* eslint-disable @typescript-eslint/tslint/config */
 import * as lib from './index';
 import * as ts from 'typescript';
+import { visitNodeAndChildren } from './visitor';
 
-type CreateFixtureArguments = {
-    sourceText: string;
-};
-
-function createFixture(createFixtureArguments: CreateFixtureArguments) {
-    const {
-        sourceText
-    } = createFixtureArguments;
-    ts.createSourceFile('filename.tsx', sourceText, ts.ScriptTarget.Latest);
+function transform(sourceText: string) {
+    const sourceFile = ts.createSourceFile('filename.tsx', sourceText, ts.ScriptTarget.Latest, true, ts.ScriptKind.TSX);
+    visitNodeAndChildren(sourceFile, <any>undefined, <any>undefined);
+    return sourceFile.getText();
 }
 
 it('smoke test', () => {
     expect(lib).toBeTruthy();
+    expect(visitNodeAndChildren).toBeTruthy();
+    expect(transform(';')).toEqual(';');
+});
+
+fit('should be removed it call expression', () => {
+    debugger;
+    expect(transform('it()')).toEqual('');
 });
 
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,8 @@
 import * as ts from 'typescript';
+import { visitNodeAndChildren } from './visitor';
 
 function typescriptTransformUnspec(program: ts.Program): ts.TransformerFactory<ts.SourceFile> {
     return (context) => (file) => visitNodeAndChildren(file, program, context);
-}
-
-function visitNodeAndChildren(node: ts.Node, program: ts.Program, context: ts.TransformationContext): ts.SourceFile {
-    return ts.visitEachChild(visitNode(node), (childNode) => visitNodeAndChildren(childNode, program, context), context) as ts.SourceFile;
-}
-
-function visitNode(node: ts.Node): ts.Node | undefined {
-    if (isSpecExpressionStatement(node)) {
-        return undefined;
-    }
-    return node;
-}
-
-function isSpecExpressionStatement(node: ts.Node) {
-    return (ts.isExpressionStatement(node) && ts.isCallExpression(node.expression)
-        && ts.isIdentifier(node.expression.expression)
-        && node.expression.expression.escapedText === 'it');
 }
 
 module.exports = typescriptTransformUnspec;
